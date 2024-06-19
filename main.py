d5e5b827c1ea52a9c31ea021316264bd6f9d984c
fajlopp <fmoszczynski@gmail.com>
2024-06-20 01:07:39 +0200

    Changed a lot, also added error handling.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    try:
+        text = get_book_text(book_path)
+        count_words = count_words_in_file(text)
+        print(count_words)
+    except FileNotFoundError:
+        print(f"Error: The file {book_path} was not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
+def count_words_in_file(text):
+    words = text.split()
+    return len(words)
 
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
